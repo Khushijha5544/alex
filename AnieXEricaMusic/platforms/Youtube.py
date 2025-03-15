55294debc144707932d8ee63700a3a84ae48d122
Khushijha5544 <133341324+Khushijha5544@users.noreply.github.com>
2025-03-15 09:00:43 +0530

    Update Youtube.py

--- AnieXEricaMusic/platforms/Youtube.py
+++ AnieXEricaMusic/platforms/Youtube.py
@@ -9,7 +9,7 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 # Yaha apni YouTube API key daalein
-YOUTUBE_API_KEY = "YOUR_YOUTUBE_API_KEY"
+YOUTUBE_API_KEY = "8170254854:AAFS0rbC-_rpX_FXaigXhi6uNEvr98SEgC4"
 
 
 async def shell_cmd(cmd):
